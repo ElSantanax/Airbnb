262ebc9e2b9146e710f3fd7f83b9198b9f1e831c
ElSantanax <josesantanaserrano130598@gmail.com>
2025-06-12 16:57:47 -0500

    Refactor SCSS and Gulp tasks for improved readability and consistency

--- gulpfile.js
+++ gulpfile.js
@@ -12,21 +12,21 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
-function css( done ) {
+function css(done) {
     src('src/scss/app.scss')
-        .pipe( sourcemaps.init() )
-        .pipe( sass() )
-        .pipe( postcss([ autoprefixer(), cssnano() ]) )
-        .pipe( sourcemaps.write('.'))
-        .pipe( dest('build/css') )
+        .pipe(sourcemaps.init())
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'))
 
     done();
 }
 
 function imagenes() {
     return src('src/img/**/*')
-        .pipe( imagemin({ optimizationLevel: 3 }) )
-        .pipe( dest('build/img') )
+        .pipe(imagemin({ optimizationLevel: 3 }))
+        .pipe(dest('build/img'))
 }
 
 function versionWebp() {
@@ -34,27 +34,26 @@ function versionWebp() {
         quality: 50
     }
     return src('src/img/**/*.{png,jpg}')
-        .pipe( webp( opciones ) )
-        .pipe( dest('build/img') )
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
 }
 function versionAvif() {
     const opciones = {
         quality: 50
     }
     return src('src/img/**/*.{png,jpg}')
-        .pipe( avif( opciones ) )
-        .pipe( dest('build/img'))
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
 }
 
 function dev() {
-    watch( 'src/scss/**/*.scss', css );
-    watch( 'src/img/**/*', imagenes );
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
 }
-
 
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.default = series( imagenes, versionWebp, versionAvif, css, dev  );
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
